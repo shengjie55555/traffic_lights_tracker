26f098c051e75aa9dbb5be7ce4579b7c3a16e2d8
shengjie55555 <shengjie55555@gmail.com>
2021-10-28 19:27:46 +0800

    add ros driver of camera

--- src/get_camera/scripts/camera_subscriber.py
+++ src/get_camera/scripts/camera_subscriber.py
@@ -14,31 +14,20 @@ import cv2
 
 
 def callback(data):
-    # define picture to_down' coefficient of ratio
     print(float(data.header.stamp.secs) + float(data.header.stamp.nsecs) * 1e-9)
-    scaling_factor = 1
-    global bridge
-    # global fourcc
-    # global outfile
-    cv_img = bridge.imgmsg_to_cv2(data, "bgr8")
-    # outfile.write(cv_img)
+    cv_img = np.frombuffer(data.data, dtype=np.uint8).reshape(data.height, data.width, -1)
     cv2.imshow("frame", cv_img)
     cv2.waitKey(3)
 
 
 def displayWebcam():
     rospy.init_node('webcam_display', anonymous=True)
-
-    # make a video_object and init the video object
-    global bridge
-    # global fourcc
-    # global outfile
-    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    # outfile = cv2.VideoWriter("/home/sheng/test.avi", fourcc, 30, (640, 480))
-    bridge = CvBridge()
     rospy.Subscriber('camera/rgb/image_raw', Image, callback)
     rospy.spin()
 
 
 if __name__ == '__main__':
-    displayWebcam()
+    try:
+        displayWebcam()
+    except rospy.ROSInterruptException:
+        pass
