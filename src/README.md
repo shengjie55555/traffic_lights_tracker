8e734f574d0df2c92ad347c10f3deef13e586931
shengjie55555 <shengjie55555@gmail.com>
2021-11-17 08:58:15 +0800

    init

--- src/README.md
+++ src/README.md
@@ -1,24 +1,6 @@
 # Traffic Lights Tracker
-## 1 系统组成
-整个系统包括三个模块：检测、跟踪、滤波和匹配。
-### 1.1 检测与跟踪
-参考该仓库[YOLOv5+DeepSORT](https://github.com/mikel-brostrom/Yolov5_DeepSort_Pytorch).主要改动在于增加了track的属性:
-```python
-self.box = box
-self.score = score
-self.cls = []
-if cls is not None:
-    self.cls.append(cls)
-```
-其中将cls变成列表，用于保存多帧的检测结果，取最新5帧的检测结果中的众数作为该目标id检测结果，这是第一次滤波。
-### 1.2 滤波与匹配
-根据规划模块发送的交通灯个数num，将检测结果和实际感兴趣的交通灯进行匹配。  
-step1：先按照左上顶点的y坐标**从小到大**（保证搜索到的第一个目标为感兴趣的交通灯）排序，然后从上到下搜索，直到找到boxes，计算找到的boxes平均的y坐标:y0，再次搜索[0, y0+10]区域内的boxes  
-step2：如果个数大于num，选择概率最大的num个，用相对位置从左到右匹配  
-step3：如果个数小与num，首先根据id匹配之前的结果，新出现的目标则先**从左到右**（初始化后全为0，此时计算的最佳匹配结果都是第0个）排序后，按照和之前结果的距离匹配  
-匹配完成后，保存最新的5帧结果，取众数进行第二次滤波
-## 2 代码解释
-### 2.1 get_camera
+## 1 代码解释
+### 1.1 get_camera
 ```shell
 camera_publisher: 加载本地视频文件，发送/camera/rgb/image_pointgrey
 camera_subscriber: 订阅/camera/rgb/image_pointgrey
@@ -27,26 +9,11 @@ ros_driver_gmsl2_camera: gmsl2相机的ros驱动
 video: 采集工业相机的数据，保存成本地视频
 ```
 备注：由于ros版本默认采用python2，因此无法使用cv_bridge
-### 2.2 tracker
+### 1.2 detector
 ```shell
-pointgrey_visual_detection: 订阅/camera/rgb/image_pointgrey,再进行检测
-gmsl2_visual_detection: 订阅/camera/rgb/image_gmsl2,再进行检测
+detector: 订阅/camera/rgb/image_pointgrey,再进行检测
 ```
-使用时需要通过Traffic_Light_Pos_Pub.cpp发送交通灯个数，数量为argv[0]
-```shell
-rosrun tracker tracker 2  # num = 2
-```  
-## 3 采集数据
-方法1：
-```shell
-rosrun get_camera video.py  # 直接打开驱动，保存视频
-```
-方法2：
-```shell
-rosrun get_camera ros_driver_camera.py  # 打开驱动，发布话题
-rosrun get_camera camera_subscriber.py  # 订阅话题，如果要保存视频，在该文件内搜索todo修改路径
-```
-## 4 使用步骤
+## 2 使用步骤
 步骤1：下载仓库，编译
 ```shell
 cd Traffic_Lights_Tracker
@@ -55,15 +22,14 @@ source ./devel/setup.bash
 ```
 步骤2：拷贝权重
 ```shell
-cp /path_to_yolo_weights/best.pt ./src/tracker/scripts/
-cp /path_to_deep_sort_weights/ckpt.t7 ./src/tracker/scripts/
+cp /path_to_yolo_weights/best.pt ./src/detector/scripts/
 ```
 步骤3：修改路径
 ```shell
-打开./src/tracker/scripts/common.py，搜索todo，总共有两次，修改实际权重和配置文件的路径
+打开./src/detector/scripts/detector.py  # 搜索todo，修改实际权重的路径
 ```
 步骤4：运行
 ```shell
-roslaunch tracker gmsl2_visual_detection.launch  # 打开驱动和检测程序
-rosrun tracker tracker 3  # 用于发送lights数量，0：关闭检测
+rosrun get_camera ros_driver_pointgrey_camera  # 打开相机驱动
+rosrun detector detector  # 检测
 ```
---
+++ src/detector/scripts/detector.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time : 2021/6/2 上午8:47
+# @Author :
+# @File : detector.py
+# @Software: CLion
+
+import rospy
+from sensor_msgs.msg import Image
+
+import time
+import cv2
+import torch
+from numpy import random
+import numpy as np
+
+from models.experimental import attempt_load
+from utils.general import (
+    check_img_size, non_max_suppression, scale_coords, plot_one_box, set_logging)
+from utils.torch_utils import select_device, time_synchronized
+from utils.datasets import letterbox
+
+
+def callback(data, args):
+    with torch.no_grad():
+        # Run inference
+        t0 = time.time()
+
+        # Read img from sensor_msg
+        img0 = np.frombuffer(data.data, dtype=np.uint8).reshape(data.height, data.width, -1)
+        img0 = np.ascontiguousarray(img0)
+
+        # Padded resize
+        img = letterbox(img0, new_shape=args["imgsz"])[0]
+
+        # Convert
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
+        img = np.ascontiguousarray(img)
+
+        img = torch.from_numpy(img).to(args["device"])
+        img = img.half() if args["half"] else img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        t1 = time_synchronized()
+        pred = args["model"](img, augment=args["augment"])[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, args["conf_thres"], args["iou_thres"],
+                                   classes=args["classes"], agnostic=args["agnostic_nms"])
+        t2 = time_synchronized()
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            s = '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(img0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if det is not None and len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], img0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += '%g %ss, ' % (n, args["names"][int(c)])  # add to string
+
+                    # Write results
+                    for *xyxy, conf, cls in reversed(det):
+                        if args["view_img"]:  # Add bbox to image
+                            label = '%s %.2f' % (args["names"][int(cls)], conf)
+                            plot_one_box(xyxy, img0, label=label, color=args["colors"][int(cls)], line_thickness=3)
+
+                # Print time (inference + NMS)
+                print('%sDone. (%.3fs)' % (s, t2 - t1))
+
+                # Stream results
+                if args["view_img"]:
+                    cv2.namedWindow("detect_result", 0)
+                    cv2.imshow("detect_result", img0)
+                    if cv2.waitKey(1) == ord('q'):  # q to quit
+                        raise StopIteration
+        
+        if pred is None:
+            cv2.namedWindow("original_image", 0)
+            cv2.imshow("original_image", img0)
+            if cv2.waitKey(1) == ord('q'):  # q to quit
+                raise StopIteration
+
+        print('Done. (%.3fs)' % (time.time() - t0))
+
+
+def main():
+    rospy.init_node("detector", anonymous=True)
+    opt = {
+        # todo: 修改权重的实际位置
+        "weights": "/home/sheng/code_space/python_projects/competition/Traffic_Lights_Tracker/src/detector/scripts/runs/exp1/weights/last.pt",
+        "imgsz": 640,
+        "augment": False,
+        "conf_thres": 0.4,
+        "iou_thres": 0.5,
+        "agnostic_nms": False,
+        "classes": None,
+        "view_img": True,
+        "device": ''}
+
+    # Initialize
+    set_logging()
+    device = select_device(opt["device"])
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(opt["weights"], map_location=device)  # load FP32 model
+    imgsz = check_img_size(opt["imgsz"], s=model.stride.max())  # check img_size
+    if half:
+        model.half()  # to FP16
+
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
+
+    temp = {
+        "device": device,
+        "half": half,
+        "model": model,
+        "imgsz": imgsz,
+        "names": names,
+        "colors": colors
+    }
+
+    opt.update(temp)
+
+    rospy.Subscriber('camera/rgb/image_pointgrey', Image, callback, opt, queue_size=1)
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    main()
